c6977ba3dffaf6d17eb8a35e64a64ee05655d379
ruchi-parsai <parsairuchi99@gmail.com>
2026-05-07 16:41:36 +0530

    add

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,21 +10,22 @@ def test_login_positive(page):
     assert login.is_login_successful()
 
     assert login.is_dashboard_visible()
-@pytest.mark.parametrize(
-    "username,password,scenario",
-    [
-        ("WrongUser", "admin123", "invalid"),
-        ("Admin", "wrongpass", "invalid"),
-        ("Wrong", "wrong", "invalid"),
-        ("", "", "empty"),
-    ]
-)
-def test_login_negative(page, username, password, scenario):
-    login = LoginPage(page)
-    login.login(username, password)
 
-    if scenario == "invalid":
-        assert "Invalid credentials" in login.get_invalid_credentials_error()
+# @pytest.mark.parametrize(
+#     "username,password,scenario",
+#     [
+#         ("WrongUser", "admin123", "invalid"),
+#         ("Admin", "wrongpass", "invalid"),
+#         ("Wrong", "wrong", "invalid"),
+#         ("", "", "empty"),
+#     ]
+# )
+# def test_login_negative(page, username, password, scenario):
+#     login = LoginPage(page)
+#     login.login(username, password)
 
-    elif scenario == "empty":
-        assert login.is_required_field_error_visible()
+#     if scenario == "invalid":
+#         assert "Invalid credentials" in login.get_invalid_credentials_error()
+
+#     elif scenario == "empty":
+#         assert login.is_required_field_error_visible()
